2410cf07b66ab74879ec3153493bc091fd90f3bc
Samuel Yazie <samuelwassie169@gmail.com>
2024-05-31 13:50:33 -0500

    updating gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,13 +1,12 @@
-
 function Gallery(props) {
     return (
       <div className="gallery">
         {props.images.map((url, index) => (
-          <img key={index} src={url} alt={`Gallery image ${index + 1}`} />
+          <img key={index} src={url}/>
         ))}
       </div>
     );
 }
-  
+
 export default Gallery;
 
